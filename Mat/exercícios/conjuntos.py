35bdaa2781baf78d373d6591b6b6fe2f5fcaed46
Leonardo Hubert <leotr1@hotmail.com>
2024-09-11 08:55:11 -0300

    exercício matemática discreta

---
+++ Mat/exercícios/conjuntos.py
@@ -0,0 +1,20 @@
+conjuntoA = {1,3,6,7,8}
+conjuntoB = {1,2,5,7,9}
+
+print("Conjunto A: ", conjuntoA)
+print("Conjunto B: ", conjuntoB)
+
+#União
+print("A união dos conjuntos é: ",conjuntoA|conjuntoB) 
+
+#Interseção
+print("A interseção dos conjuntos é: ", conjuntoA & conjuntoB)
+
+#Diferença A - B
+print("A diferença dos conjuntos A e B é: ", conjuntoA - conjuntoB)
+
+#Diferença B - A
+print("A diferença dos conjuntos B e A é: ", conjuntoB - conjuntoA)
+
+#Diferença Simétrica
+print("A diferença simétrica entre os conjuntos é: ", conjuntoA ^ conjuntoB)
