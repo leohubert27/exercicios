4afd10be619d3e0b5f79a6c11a772289a80cb393
Leonardo Hubert <leotr1@hotmail.com>
2024-10-10 11:01:10 -0300

    exercício média/moda/mediana

---
+++ dw2/atividades/media_moda_mediana/main.js
@@ -0,0 +1,63 @@
+"use strict";
+// Função para calcular a média
+function calcularMedia(numeros) {
+    if (numeros.length === 0) {
+        return 'O array está vazio!';
+    }
+    const soma = numeros.reduce((acc, num) => acc + num, 0);
+    return soma / numeros.length;
+}
+// Função para calcular a mediana
+function calcularMediana(numeros) {
+    if (numeros.length === 0) {
+        return 'O array está vazio!';
+    }
+    const numerosOrdenados = [...numeros].sort((a, b) => a - b);
+    const meio = Math.floor(numerosOrdenados.length / 2);
+    if (numerosOrdenados.length % 2 === 0) {
+        return (numerosOrdenados[meio - 1] + numerosOrdenados[meio]) / 2;
+    }
+    else {
+        return numerosOrdenados[meio];
+    }
+}
+// Função para calcular a moda
+function calcularModa(numeros) {
+    if (numeros.length === 0) {
+        return 'O array está vazio!';
+    }
+    const frequencias = {};
+    numeros.forEach(num => {
+        frequencias[num] = (frequencias[num] || 0) + 1;
+    });
+    const maxFrequencia = Math.max(...Object.values(frequencias));
+    const modas = Object.keys(frequencias).filter(num => frequencias[num] === maxFrequencia).map(Number);
+    return modas;
+}
+// Função principal que executa os cálculos e exibe os resultados
+function calcularEstatisticas(numeros) {
+    const media = calcularMedia(numeros);
+    const mediana = calcularMediana(numeros);
+    const moda = calcularModa(numeros);
+    if (typeof media === 'string') {
+        console.log(media);
+    }
+    else {
+        console.log(`Média: ${media}`);
+    }
+    if (typeof mediana === 'string') {
+        console.log(mediana);
+    }
+    else {
+        console.log(`Mediana: ${mediana}`);
+    }
+    if (typeof moda === 'string') {
+        console.log(moda);
+    }
+    else {
+        console.log(`Moda: ${moda.join(', ')}`);
+    }
+}
+// Exemplo de uso
+const listaNumeros = [1, 3, 4, 4, 7, 9, 4, 2, 3];
+calcularEstatisticas(listaNumeros);
